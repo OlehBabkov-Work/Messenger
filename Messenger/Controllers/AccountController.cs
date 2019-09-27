971e8c951ab2a98161994a40e3be9674f8b0b4be
OlehBabkov-Work <oleh.babkov@yahoo.com>
2019-09-27 14:19:16 +0300

    logic of AccountController was modified; HomeController is added

--- Messenger/Controllers/AccountController.cs
+++ Messenger/Controllers/AccountController.cs
@@ -19,7 +19,54 @@ namespace Messenger.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginModel model)
         {
+            if (ModelState.IsValid)
+            {
+                // Searching user in db
+                User user = db.Users.FirstOrDefault(u => u.Login == model.Name && u.Password == model.Password);
 
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "There is no user with such login and password");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = db.Users.FirstOrDefault(u => u.Login == model.Name && u.Password == model.Password);
+                if (user == null)
+                {
+                    db.Users.Add(new User { Login = model.Name, Password = model.Password, RoleId = 2 });
+                    db.SaveChanges();
+                    user = db.Users.Where(u => u.Login == model.Name && u.Password == model.Password).FirstOrDefault();
+                    if (user != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Name, true);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                    ModelState.AddModelError("", "User with such login and password already exists");
+            }
+            return View(model);
+        }
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
