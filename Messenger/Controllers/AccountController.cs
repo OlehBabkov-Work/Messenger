c815197cdd119b1f3dbd6bc953e99418f67655db
OlehBabkov-Work <oleh.babkov@yahoo.com>
2019-09-27 13:05:01 +0300

    web.config is changed, AccountController is added, Login and Register models are added

---
+++ Messenger/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Security;
+using System.Web;
+using System.Web.Mvc;
+using Messenger.Models;
+
+namespace Messenger.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserContext db = new UserContext();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+
+        }
+    }
+}
---
+++ Messenger/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Messenger.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords are different")]
+        public string ConfirmPassword { get; set; }
+    }
+}
